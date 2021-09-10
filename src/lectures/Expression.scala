754a326157d22a47c65646cb85edc2efc8fe60ba
Naresh <naresh14334@gmail.com>
2021-09-10 19:48:03 +0530

    commited

---
+++ src/lectures/Expression.scala
@@ -0,0 +1,8 @@
+package lectures
+
+object Expression extends App {
+
+  val aCondition = true
+  val aConditionValue = if(true) 5 else 3
+  println(aConditionValue)
+}
---
+++ src/lectures/ValuesVariables.scala
@@ -0,0 +1,17 @@
+package lectures
+
+//import sun.font.TrueTypeFont
+
+object ValuesVariables extends App {
+  val x:Int=18
+  val str:String = "kliop"
+  val num:Double=109.23
+  val bool:Boolean = true
+  val flt:Float = 12121.2f
+  var intx = 12
+  val str1:List[String] = List("jik","jkl")
+  intx = -33333
+  println(x,str,num,bool,flt,intx,str1)
+
+
+}
